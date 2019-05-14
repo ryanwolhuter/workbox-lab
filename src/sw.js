311ad9e5741cfed644ce2c749c07364759a90e12
ryanwolhuter <dev@ryanwolhuter.com>
2019-05-14 10:30:07 +0200

    Add service worker for precaching and update gulpfile

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,8 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
+if (workbox) {
+  console.log(`Sweet, it's working.`);
+  workbox.precaching.precacheAndRoute([]);
+} else {
+  console.log(`Sorry mate, no luck with the service worker.`);
+}
