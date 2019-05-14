a6c53b879e4be7c4b3329c836f626dc10e125f81
ryanwolhuter <dev@ryanwolhuter.com>
2019-05-14 10:46:54 +0200

    Add image precaching

--- src/sw.js
+++ src/sw.js
@@ -6,3 +6,28 @@ if (workbox) {
 } else {
   console.log(`Sorry mate, no luck with the service worker.`);
 }
+
+workbox.routing.registerRoute(
+  /(.*)articles(.*)\.(?:png|gif|jpg)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/images/icon/*'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'icon-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 5
+      })
+    ]
+  })
+);
